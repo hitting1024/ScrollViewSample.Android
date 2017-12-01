00143c8b0f996c2debe970564f32dd1f18a57512
IOKA <hit0apps@gmail.com>
2017-12-01 23:31:02 +0900

    add scroll action

--- app/src/main/java/jp/hitting/scrollviewsample/MainActivity.kt
+++ app/src/main/java/jp/hitting/scrollviewsample/MainActivity.kt
@@ -1,12 +1,30 @@
 package jp.hitting.scrollviewsample
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.ScrollView
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        this.addAction()
     }
+
+    private fun addAction() {
+        val scrollView = this.findViewById<ScrollView>(R.id.scrollView)
+        val scrollToBottomButton = scrollView.scrollToBottomButton
+        val scrollToTopButton = scrollView.scrollToTopButton
+
+        scrollToBottomButton.setOnClickListener {
+            scrollView.scrollTo(0, scrollToTopButton.y.toInt())
+        }
+        scrollToTopButton.setOnClickListener {
+            scrollView.scrollTo(0, 0)
+        }
+    }
+
 }
