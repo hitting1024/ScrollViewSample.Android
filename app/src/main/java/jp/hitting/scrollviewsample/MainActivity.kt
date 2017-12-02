d0cb6d9b362b84fbe489d8ceeb349bb26aa98efe
IOKA <hit0apps@gmail.com>
2017-12-02 19:19:18 +0900

    add smooth scroll action

--- app/src/main/java/jp/hitting/scrollviewsample/MainActivity.kt
+++ app/src/main/java/jp/hitting/scrollviewsample/MainActivity.kt
@@ -3,6 +3,7 @@ package jp.hitting.scrollviewsample
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.ScrollView
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
@@ -18,12 +19,20 @@ class MainActivity : AppCompatActivity() {
         val scrollView = this.findViewById<ScrollView>(R.id.scrollView)
         val scrollToBottomButton = scrollView.scrollToBottomButton
         val scrollToTopButton = scrollView.scrollToTopButton
+        val smoothScrollToBottomButton = scrollView.smoothScrollToBottomButton
 
         scrollToBottomButton.setOnClickListener {
             scrollView.scrollTo(0, scrollToTopButton.y.toInt())
         }
         scrollToTopButton.setOnClickListener {
             scrollView.scrollTo(0, 0)
+        }
+
+        smoothScrollToBottomButton.setOnClickListener {
+            scrollView.smoothScrollTo(0, scrollToTopButton.y.toInt())
+        }
+        smoothScrollToTopButton.setOnClickListener {
+            scrollView.smoothScrollTo(0, 0)
         }
     }
 
