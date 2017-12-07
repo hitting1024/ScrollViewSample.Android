adc3b0d757a0a3c2e2b26ceefd5a88a865081940
IOKA <hit0apps@gmail.com>
2017-12-07 22:53:37 +0900

    transtion to pager view

--- app/src/main/java/jp/hitting/scrollviewsample/MainActivity.kt
+++ app/src/main/java/jp/hitting/scrollviewsample/MainActivity.kt
@@ -1,7 +1,10 @@
 package jp.hitting.scrollviewsample
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ScrollView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
@@ -34,6 +37,21 @@ class MainActivity : AppCompatActivity() {
         smoothScrollToTopButton.setOnClickListener {
             scrollView.smoothScrollTo(0, 0)
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        this.menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.pager -> {
+                this.startActivity(Intent(this, PagerActivity::class.java))
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
