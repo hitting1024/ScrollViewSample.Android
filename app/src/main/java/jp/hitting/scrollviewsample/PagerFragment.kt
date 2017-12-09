ba4999f6c37dc94137ef41f21629f621507040e3
IOKA <hit0apps@gmail.com>
2017-12-09 23:05:37 +0900

    add scroll action at pager

--- app/src/main/java/jp/hitting/scrollviewsample/PagerFragment.kt
+++ app/src/main/java/jp/hitting/scrollviewsample/PagerFragment.kt
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ScrollView
+import kotlinx.android.synthetic.main.fragment_pager.view.*
 
 class PagerFragment : Fragment() {
 
@@ -14,7 +16,30 @@ class PagerFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val view = inflater.inflate(R.layout.fragment_pager, null)
         view.setBackgroundColor(this.color)
+        this.addAction(view)
         return view
     }
-    
+
+    private fun addAction(view: View) {
+        val scrollView = view.findViewById<ScrollView>(R.id.scrollView)
+        val scrollToBottomButton = scrollView.scrollToBottomButton
+        val scrollToTopButton = scrollView.scrollToTopButton
+        val smoothScrollToBottomButton = scrollView.smoothScrollToBottomButton
+        val smoothScrollToTopButton = scrollView.smoothScrollToTopButton
+
+        scrollToBottomButton.setOnClickListener {
+            scrollView.scrollTo(0, scrollToTopButton.y.toInt())
+        }
+        scrollToTopButton.setOnClickListener {
+            scrollView.scrollTo(0, 0)
+        }
+
+        smoothScrollToBottomButton.setOnClickListener {
+            scrollView.smoothScrollTo(0, scrollToTopButton.y.toInt())
+        }
+        smoothScrollToTopButton.setOnClickListener {
+            scrollView.smoothScrollTo(0, 0)
+        }
+    }
+
 }
